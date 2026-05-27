a1c70d0480cc623dde596fc12f4aa3dc7a1a165e
Sam <stewartsamaira@gmail.com>
2026-05-26 20:23:02 -0400

    Add: lonely-pixel-i - submission-0

---
+++ Data Structures & Algorithms/lonely-pixel-i/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findLonelyPixel(self, picture: List[List[str]]) -> int:
+        n = len(picture)
+        m = len(picture[0])
+
+        # Arrays to store the count of black cells in rows and columns
+        row_count = [0] * n
+        column_count = [0] * m
+
+        for i in range(n):
+            for j in range(m):
+                if picture[i][j] == "B":
+                    row_count[i] += 1
+                    column_count[j] += 1
+        
+        answer = 0
+
+        for i in range(n):
+            for j in range(m):
+                # If its a lonely cell, the current cell will be black 
+                # and the count of cells in its row and column will be 1
+                if picture[i][j] == "B" and row_count[i] == 1 and column_count[j] == 1:
+                    answer += 1
+        
+        return answer
+
+            
+                
+
+                
+       
